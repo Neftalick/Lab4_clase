1f563da78e26793790d05b50f0a89823d93e2ab2
Ronny Pastor <ronnypastorkolmakov@gmail.com>
2022-04-18 10:30:11 -0500

    Initial commit

---
+++ src/main/java/com/example/lab4_clase/Lab4ClaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab4_clase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4ClaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab4ClaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab4_clase/Lab4ClaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab4_clase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab4ClaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
